4b1602fc47ef7601e29842f6ef6fc74cf1e7ebc1
Sergio Pereira <sergio.pereira@strat.pt>
2011-03-31 18:38:31 +0100

    Added backend logic in pseudo code

--- upload.php
+++ upload.php
@@ -1,8 +1,61 @@
 <?php
 
+require_once("includes/database.php");
+
 $errors = array();
 $data = "";
 $success = "true";
+$db = new database();
+
+
+switch($_REQUEST['action']) {
+
+
+	case "putUser":
+		
+		$user = array('username' => $_POST['username'], 'useremail' => $_POST['useremail']);
+		$db->
+		
+		break;
+		
+		
+    case "putFile":
+
+		$file_temp = $_FILES['file']['tmp_name'];
+		$file_name = $_FILES['file']['name'];
+
+		//$file_path = $_SERVER['DOCUMENT_ROOT']."/uploads";
+		 $file_path = "uploads";
+
+		//checks for duplicate files
+		if(!file_exists($file_path."/".$file_name)) {
+
+			 //complete upload
+			 $filestatus = move_uploaded_file($file_temp,$file_path."/".$file_name);
+
+			 if(!$filestatus) {
+			 $success = "false";
+			 array_push($errors,"Upload failed. Please try again. ".$file_path."/".$file_name);
+			 }
+
+		}
+		else {
+		$success = "false";
+		array_push($errors,"File already exists on server.");
+		}
+
+		break;
+
+
+    default:
+		$success = "false";
+		array_push($errors,"No action was requested.");
+
+}
+
+
+return_result($success, $errors, $data);
+
 
 function return_result($success,$errors,$data) {
 	echo("<?xml version=\"1.0\" encoding=\"utf-8\"?>");	
@@ -24,41 +77,6 @@ function echo_errors($errors) {
 	}
 }
 
-switch($_REQUEST['action']) {
 
-    case "upload":
-
-    $file_temp = $_FILES['file']['tmp_name'];
-    $file_name = $_FILES['file']['name'];
-
-    //$file_path = $_SERVER['DOCUMENT_ROOT']."/uploads";
-	 $file_path = "uploads";
-
-    //checks for duplicate files
-    if(!file_exists($file_path."/".$file_name)) {
-
-         //complete upload
-         $filestatus = move_uploaded_file($file_temp,$file_path."/".$file_name);
-
-         if(!$filestatus) {
-         $success = "false";
-         array_push($errors,"Upload failed. Please try again. ".$file_path."/".$file_name);
-         }
-
-    }
-    else {
-    $success = "false";
-    array_push($errors,"File already exists on server.");
-    }
-
-    break;
-
-    default:
-    $success = "false";
-    array_push($errors,"No action was requested.");
-
-}
-
-return_result($success,$errors,$data);
 
 ?>
